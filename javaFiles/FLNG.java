4ae64e152f5de12bd11e22a92a2db335704294ef
伊藤航大0815 <itokoudai@itouwatarudai-no-MacBook-Air-2.local>
2015-01-09 11:14:53 +0900

    コメント少し変えた

--- javaFiles/FLNG.java
+++ javaFiles/FLNG.java
@@ -2,12 +2,12 @@ package javaFiles;
 
 
 public class FLNG {
-	private final double Q = 2000;//供給量
+	private final double Q = 2000;//時間あたり生産量
 	private double W0;//キャパシティ
 	private double T1 = 54;//積載時間
-	private boolean vacant = true;//積載できるか。falseなら他の船が使用中
+	private boolean vacant = true;//係船できるか。falseなら他の船が使用中
 	private double amount;//LNGの量
-	public FLNG(double input_W0) {
+	public FLNG(double input_W0) { 
 		this.W0 = input_W0;
 	}
 	
